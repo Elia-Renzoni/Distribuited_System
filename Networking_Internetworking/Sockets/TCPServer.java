b7ba6fdfc299243b124274cd75074c8036f64def
Elia Renzoni <118525453+Elia-Renzoni@users.noreply.github.com>
2023-10-28 09:12:06 +0200

    Update TCPServer.java

--- Networking_Internetworking/Sockets/TCPServer.java
+++ Networking_Internetworking/Sockets/TCPServer.java
@@ -10,14 +10,14 @@ import java.io.*;
 public class TCPServer {
     public static void main(String[] args) throws IOException {
         ServerSocket server = new ServerSocket(1280);
-        Socket client = server.accept();
-        OutputStream serverComm = client.getOutputStream();
+        Socket s = server.accept();
+        OutputStream serverComm = s.getOutputStream();
         DataOutputStream socketDos = new DataOutputStream(serverComm);
         socketDos.writeUTF("Messaggio dal Server");
         
         socketDos.close();
         serverComm.close();
-        client.close();
+        s.close();
         server.close();
     }
 }
